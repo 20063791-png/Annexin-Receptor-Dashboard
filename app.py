8cd92c392fabdf7c6a2b83472f8f6f7624b4fc20
20063791-png <20063791@studenti.uniupo.it>
2026-06-10 18:31:12 +0200

    Add files via upload

--- app.py
+++ app.py
@@ -1,1 +1,11 @@
- 
+
+import streamlit as st
+
+st.title("Annexin Receptor Dashboard")
+
+st.write("Dashboard is working!")
+
+gene = st.text_input("Enter Gene Symbol")
+
+if gene:
+    st.write("You searched:", gene)
